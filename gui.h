da3ad4a4d4989923315e0528b17a92746fd5a540
Thomas POTIER <thomas.potier.92@hotmail.fr>
2018-04-16 01:44:34 +0200

    Adding more streets

--- gui.h
+++ gui.h
@@ -34,6 +34,23 @@ case_t CASES[] = {
     {(char *)"Foch", 6, TYPE_STREET, -1, 100, 100},
     {(char *)"Champs-Elysees", 7, TYPE_STREET, -1, 100, 100},
     {(char *)"La Paix", 7, TYPE_STREET, -1, 100, 100},
+    
+    {(char *)"Belleville", 0, TYPE_STREET, -1, 100, 100},
+    {(char *)"Lecourbe", 0, TYPE_STREET, -1, 100, 100},
+    {(char *)"Vaugirard", 1, TYPE_STREET, -1, 100, 100},
+    {(char *)"Courcelles", 1, TYPE_STREET, -1, 100, 100},
+    {(char *)"La Vilette", 2, TYPE_STREET, -1, 100, 100},
+    {(char *)"Neuilly", 2, TYPE_STREET, -1, 100, 100},
+    {(char *)"Mozart", 3, TYPE_STREET, -1, 100, 100},
+    {(char *)"Saint-Michel", 3, TYPE_STREET, -1, 100, 100},
+    {(char *)"Matignon", 4, TYPE_STREET, -1, 100, 100},
+    {(char *)"Malesherbes", 4, TYPE_STREET, -1, 100, 100},
+    {(char *)"Saint-Honore", 5, TYPE_STREET, -1, 100, 100},
+    {(char *)"La Bourse", 5, TYPE_STREET, -1, 100, 100},
+    {(char *)"Breteuil", 6, TYPE_STREET, -1, 100, 100},
+    {(char *)"Foch", 6, TYPE_STREET, -1, 100, 100},
+    {(char *)"Champs-Elysees", 7, TYPE_STREET, -1, 100, 100},
+    {(char *)"La Paix", 7, TYPE_STREET, -1, 100, 100},
 };
 
 void    display_board(board_t *board);
