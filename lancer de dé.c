df4e73059b673cf2cd024c9cfee023a1befe2333
OusmannN <38245073+OusmannN@users.noreply.github.com>
2018-04-15 20:19:33 +0200

    Add files via upload

---
+++ lancer de dé.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+#include<ansi_c.h>
+#include<time.h>   
+
+int frand_1(void){
+	
+	return rand() % (6)+1;
+}
+
+int main(void){
+	srand(time(NULL));
+	for(int i=0;i<100;i++){
+		printf("%d",frand_1());
+	}
+	return 0;
+}
+		
+	
+	
