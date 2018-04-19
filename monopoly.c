54ad1921a0d777ec3d8a7b9395040960a1035c6b
Hugo <hugocorbin91@gmail.com>
2018-04-19 16:11:27 +0200

    Ajout du debut du jeu

--- monopoly.c
+++ monopoly.c
@@ -2,23 +2,39 @@
 
 int main(void)
 {
+	int number = 0;
+	char name[100];
+    int i;
+
     // creating board
     board_t  *board;
     if (!(board = (board_t *)malloc(sizeof(board_t))))
         exit(-1);
     // setting up saces
     board->cases = CASES;
-   
+
     // set number of players
-    board->player_number = 1;
-    
-    //// creating a player
-    player_t *p1;
-    if (!(p1 = (player_t *)malloc(sizeof(player_t))))
+	printf("Combien de joueurs ? : ");
+	scanf("&s",*number);
+
+    board->player_number = number;
+
+    if (!(board->players = (player_t**)malloc(sizeof(player_t*) * board->player_number)))
         exit(-1);
-    p1->money = 100;
-    p1->name = (char *)"Thomas";
-    p1->position = 3;
+
+    //// creating a player
+	for (i = 0 ; i < number ; i++)
+    {
+        player_t *p1;
+        fgets(name,100,stdin);
+        if (!(p1 = (player_t *)malloc(sizeof(player_t))))
+            exit(-1);
+        p1->money = 100;
+        p1->name = (char *)name;
+        p1->position = 3;
+        board->players[i]=p1;
+    }
+
 
     // adding player to board
     board->players[0] = p1;
